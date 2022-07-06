36ef7ace486c72e16341f5f64000f5d609880da3
mohagussetiaone <mohagussetiaone@gmail.com>
2022-07-06 09:51:29 +0700

    Membuat Route sederhana untuk path Home dan About

--- src/App.js
+++ src/App.js
@@ -1,37 +1,78 @@
 import React, { useState } from "react";
 import FirstComponent from "./components/FirstComponent";
 import SecondComponent from "./components/SecondComponent";
+// import Routes, Route dan Link pada react-router-dom
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
-  // Membuat State
-  // asumsi hanya ada first dan second
-  const [position, setPosition] = useState("first");
-  // Membuat handler onClick
-  const handlerClick = (event, position) => {
-    // mencegah refresh page dengan event dan disertai dengan preventDefault
-    event.preventDefault();
-    setPosition(position);
-  };
+  // // Membuat State
+  // // asumsi hanya ada first dan second
+  // const [position, setPosition] = useState("first");
+  // // Membuat handler onClick
+  // const handlerClick = (event, position) => {
+  //   // mencegah refresh page dengan event dan disertai dengan preventDefault
+  //   event.preventDefault();
+  //   setPosition(position);
+  // };
 
   return (
     <div className="App">
-      {/* Membuat navbar sederhana */}
+      {/* Mendiskripsikan Routes */}
+      <Routes>
+        {/* Mendiskripsikan Routenya atau jalur rute nya*/}
+        {/* menerima atribute dengan path dan elementnya
+        itu berarti jika path sebagai rute misal kan local:3000/ itu akan menampilkan komponen apa? */}
+        {/* Dibawah digunakan untuk component Home (Hanya untuk pembelajaran)*/}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+
+      {/* Membuat navbar sederhana
       <span style={{ display: "flex", gap: "0.5em" }}>
         {/* menggunakan onClick */}
-        <a href="/" onClick={(event) => handlerClick(event, "first")}>
-          Component Pertama
-        </a>
-        <a href="/" onClick={(event) => handlerClick(event, "second")}>
-          Component Kedua
-        </a>
-      </span>
+      {/* <a href="/" onClick={(event) => handlerClick(event, "first")}> */}
+      {/* Component Pertama  */}
+      {/* </a> */}
+      {/* <a href="/" onClick={(event) => handlerClick(event, "second")}> */}
+      {/* Component Kedua  */}
+      {/* </a> */}
+      {/* </span> */}
       {/* Kekurangan conditional rendering adalah saat berada pada halaman tertentu jika kira refresh maka akan kembali ke halaman utamanya */}
-      <h3>Menggunakan Conditional rendering dengan ternary</h3>
+      {/* <h3>Menggunakan Conditional rendering dengan ternary</h3> */}
       {/* Menggunakan conditional rendering dengan ternary */}
-      {position === "first" ? <FirstComponent /> : <SecondComponent />}
-      {/* <FirstComponent />
-      <SecondComponent /> */}
+      {/* {position === "first" ? <FirstComponent /> : <SecondComponent />} */}
+      {/* <FirstComponent /> */}
+      {/* <SecondComponent /> */}
     </div>
+  );
+}
+
+// Hanya untuk pembelajaran tidak untuk production
+function Home() {
+  return (
+    <>
+      <main>
+        <h1>Ayo ke About boleh klik link dibawah ini</h1>
+        <p>lorem ipsum</p>
+      </main>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </>
+  );
+}
+
+function About() {
+  return (
+    <>
+      <main>
+        <h1>Ayo ke Home boleh klik link dibawah ini</h1>
+        <p>lorem ipsum</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
   );
 }
 
