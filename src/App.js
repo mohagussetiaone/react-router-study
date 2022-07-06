e85cb8437fc6e20cfc70ebb8a347879ed7a60e71
mohagussetiaone <mohagussetiaone@gmail.com>
2022-07-06 08:51:58 +0700

    Membuat navigation bar sederhana dengan state dan anonymouse function

--- src/App.js
+++ src/App.js
@@ -6,8 +6,24 @@ function App() {
   // Membuat State
   // asumsi hanya ada first dan second
   const [position, setPosition] = useState("first");
+  // Membuat handler onClick
+  const handlerClick = (position) => {
+    setPosition(position);
+  };
+
   return (
     <div className="App">
+      {/* Membuat navbar sederhana */}
+      <span style={{ display: "flex", gap: "0.5em" }}>
+        {/* menggunakan onClick */}
+        <a href="#" onClick={() => handlerClick("first")}>
+          Component Pertama
+        </a>
+        <a href="#" onClick={() => handlerClick("second")}>
+          Component Kedua
+        </a>
+      </span>
+      <h3>Menggunakan Conditional rendering dengan ternary</h3>
       {/* Menggunakan conditional rendering dengan ternary */}
       {position === "first" ? <FirstComponent /> : <SecondComponent />}
       {/* <FirstComponent />
